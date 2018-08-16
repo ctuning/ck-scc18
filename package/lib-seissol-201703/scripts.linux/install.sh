2cf11b04b53056053e2c8585297e6864cfeb8ebd
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-08-16 23:43:27 +0200

    extra fixes

--- package/lib-seissol-201703/scripts.linux/install.sh
+++ package/lib-seissol-201703/scripts.linux/install.sh
@@ -17,6 +17,45 @@ git submodule update
 PREFIX=${INSTALL_DIR}/install
 mkdir -p $PREFIX
 
+######################################################################################
+# Print info about possible issues
+echo ""
+echo "Note that you sometimes need to upgrade your pip to the latest version"
+echo "to avoid well-known issues with user/system space installation:"
+
+SUDO="sudo "
+if [[ ${CK_PYTHON_PIP_BIN_FULL} == /home/* ]] ; then
+  SUDO=""
+fi
+
+######################################################################################
+# Check if has --system option
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --help > tmp-pip-help.tmp
+if grep -q "\-\-system" tmp-pip-help.tmp ; then
+ SYS=" --system"
+fi
+rm -f tmp-pip-help.tmp
+
+######################################################################################
+echo "Downloading and installing Python deps ..."
+echo ""
+
+EXTRA_PYTHON_SITE=${INSTALL_DIR}/python-lib
+rm -rf ${EXTRA_PYTHON_SITE}
+mkdir -p ${EXTRA_PYTHON_SITE}
+
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --ignore-installed lxml -t ${EXTRA_PYTHON_SITE}  ${SYS}
+if [ "${?}" != "0" ] ; then
+  echo "Error: installation failed!"
+  exit 1
+fi
+
+export PYTHONPATH=${EXTRA_PYTHON_SITE}:$PYTHONPATH
+
+######################################################################################
+echo "Continue installation ..."
+echo ""
+
 COMPILER_TYPE="gcc"
 if [ "${CK_CC}" == "icc" ] ; then
    COMPILER_TYPE="intel"
